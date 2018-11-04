f67d66cf171b9c11080b612a77ecdbec70cd4445
HiiamCam <tam.vir89@gmail.com>
2018-11-04 14:27:09 +0700

    Updates

---
+++ precache-manifest.c1cd6494bf5e7d464f307273b575cee8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dacbf2bb41373ec188e1f0f6f7e4e14",
+    "url": "/camportfolio/static/media/img3.2dacbf2b.jpg"
+  },
+  {
+    "revision": "86d8f8c055f7e8bdb744",
+    "url": "/camportfolio/static/css/main.ea03fde1.chunk.css"
+  },
+  {
+    "revision": "cf9d1983f9d1076513a5",
+    "url": "/camportfolio/static/js/1.cf9d1983.chunk.js"
+  },
+  {
+    "revision": "ae42b5d3ffe4bb9ce210",
+    "url": "/camportfolio/static/js/runtime~main.ae42b5d3.js"
+  },
+  {
+    "revision": "45308dfbb235927e2d3b56f1b479d411",
+    "url": "/camportfolio/static/media/img1.45308dfb.jpg"
+  },
+  {
+    "revision": "5ec06c55cc575b3baf29820287aa4d42",
+    "url": "/camportfolio/static/media/img2.5ec06c55.jpg"
+  },
+  {
+    "revision": "86d8f8c055f7e8bdb744",
+    "url": "/camportfolio/static/js/main.86d8f8c0.chunk.js"
+  },
+  {
+    "revision": "d2e93368310df078e7802c0b771f7f47",
+    "url": "/camportfolio/static/media/img4.d2e93368.jpg"
+  },
+  {
+    "revision": "1ce3b77d23e26109ede8414bc2ffdcb6",
+    "url": "/camportfolio/static/media/portrait_small.1ce3b77d.jpg"
+  },
+  {
+    "revision": "f42360a1930da8fe238b2b884ecb3937",
+    "url": "/camportfolio/static/media/portrait.f42360a1.jpg"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/camportfolio/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "86c104d5116ff202873699590c20ca7a",
+    "url": "/camportfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/camportfolio/precache-manifest.d0782e9e84fe603b989685e83c929d3e.js"
+  "/camportfolio/precache-manifest.c1cd6494bf5e7d464f307273b575cee8.js"
 );
 
 workbox.clientsClaim();
